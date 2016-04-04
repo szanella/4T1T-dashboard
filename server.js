71ed1bad49ba4f5f544f96d725b24d474db5b854
Samuele Zanella <samuele@moku.io>
2016-04-04 14:15:28 +0200

    Restore things to work locally

--- server.js
+++ server.js
@@ -26,6 +26,6 @@
     require('./app/routes/routes')(app);
 
     // listen (start app with node server.js) ======================================
-    var port = process.env.PORT || CONFIG.port;
+    var port = 8080;
     app.listen(port);
     console.log("App listening on port " + port);
