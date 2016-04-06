631efca0acc35bde88602f6db992636986c9a9b6
Samuele Zanella <samuele@moku.io>
2016-04-06 17:09:14 +0200

    Improve member controller

--- public/app/components/members/memberController.js
+++ public/app/components/members/memberController.js
@@ -2,72 +2,68 @@
   angular.module('4T1T')
     .controller('MemberCtrl', memberCtrl);
 
-    memberCtrl.$inject = ['$scope', 'Members', '$routeParams'];
-    function memberCtrl($scope, Members, $routeParams) {
-      var vm = this;
-      //retrieve the members
-      Members.getSingle($routeParams.member_id)
-        .success(function(data) {
-          vm.member = data;
-          vm.member.favouriteHeroes = vm.member.favouriteHeroes || {};
-          vm.editMode = false;
-          vm.roles = [
-            {
-              label: 'Safelane',
-              id: 'safelane'
-            },
-            {
-              label: 'Mid',
-              id: 'mid'
-            },
-            {
-              label: 'Support',
-              id: 'support'
-            },
-            {
-              label: 'Offlane',
-              id: 'offlane'
-            },
-            {
-              label: 'Roaming',
-              id: 'roaming'
-            },
-            {
-              label: 'Jungle',
-              id: 'jungle'
-            }
-          ];
+  memberCtrl.$inject = ['$scope', 'Members', '$routeParams'];
+  function memberCtrl($scope, Members, $routeParams) {
+    var vm = this;
+    vm.editMode = false;
+    vm.roles = [
+      {
+        label: 'Safelane',
+        id: 'safelane'
+      },
+      {
+        label: 'Mid',
+        id: 'mid'
+      },
+      {
+        label: 'Support',
+        id: 'support'
+      },
+      {
+        label: 'Offlane',
+        id: 'offlane'
+      },
+      {
+        label: 'Roaming',
+        id: 'roaming'
+      },
+      {
+        label: 'Jungle',
+        id: 'jungle'
+      }
+    ];
 
-          function degreeCompare(a, b) {
-            return a.degree - b.degree;
-          }
+    //retrieve the members
+    Members.getSingle($routeParams.member_id)
+      .success(function(data) {
+        vm.member = data;
+        vm.member.favouriteHeroes = vm.member.favouriteHeroes || {};
+        updateFavHeroes(vm.member.favouriteHeroes);
+      });
 
-          function updateFavHeroes(fav) {
-            vm.member.favouriteHeroes = fav;
-            vm.member.favouriteHeroes.sort(degreeCompare);
+    function degreeCompare(a, b) {
+      return a.degree - b.degree;
+    }
 
-            vm.member.favByRole = {
-              'safelane': vm.member.favouriteHeroes.filter(function(hero) {return hero.position === 'safelane'}),
-              'mid': vm.member.favouriteHeroes.filter(function(hero) {return hero.position === 'mid'}),
-              'support': vm.member.favouriteHeroes.filter(function(hero) {return hero.position === 'support'}),
-              'offlane': vm.member.favouriteHeroes.filter(function(hero) {return hero.position === 'offlane'}),
-              'roaming': vm.member.favouriteHeroes.filter(function(hero) {return hero.position === 'roaming'}),
-              'jungle': vm.member.favouriteHeroes.filter(function(hero) {return hero.position === 'jungle'})
-            };
-          }
+    function updateFavHeroes(fav) {
+      vm.member.favouriteHeroes = fav;
+      vm.member.favouriteHeroes.sort(degreeCompare);
 
-          updateFavHeroes(vm.member.favouriteHeroes);
+      vm.member.favByRole = {};
+      vm.roles.forEach(function(role) {
+        vm.member.favByRole[role.id] = vm.member.favouriteHeroes.filter(function(hero) {return hero.position === role.id;});
+      });
+    }
 
-          vm.toggleEditMode = function() {
-            vm.editMode = !vm.editMode;
-          };
-
-          vm.deleteHeroFromFavourites = function(role, hero) {
-            Members.deleteHeroFromFavourites(vm.member._id, role, hero)
-              .success(function(data) {
-                updateFavHeroes(data);
-              });
-          }
-        });
+    vm.toggleEditMode = function() {
+      vm.editMode = !vm.editMode;
     };
+
+    vm.deleteHeroFromFavourites = function(role, hero) {
+      Members.deleteHeroFromFavourites(vm.member._id, role, hero)
+      .success(function(data) {
+        updateFavHeroes(data);
+      });
+    }
+  };
 })();
