69f11010b413ee891fe7333cc221778f927ac39a
Samuele Zanella <samuele@moku.io>
2016-04-08 16:13:53 +0200

    Fix mistake

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -2,7 +2,6 @@
   angular.module('4T1T', ['ngRoute', 'toastr'])
    .run(function($rootScope, auth, $location) {
      $rootScope.$on('$routeChangeStart', function (event, next) {
-       console.log("CHANGE LOCATION");
         if (!auth.isAuthed() && !next.isLogin) {
             $location.path('/login');
         }
