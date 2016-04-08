870115f61cf70fcee30ed77f6331fea89cb16e2d
Samuele Zanella <samuele@moku.io>
2016-04-08 10:44:57 +0200

    Add missing declaration

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,10 @@
 module.exports = function(app) {
-  var apiRoutes = express.Router();
+  var express  = require('express'), apiRoutes = express.Router();
   require('./heroes.routes.js')(apiRoutes);
   require('./members.routes.js')(apiRoutes);
 
   app.use('/api', apiRoutes);
-  
+
   app.get('*', function(req, res) {
       res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
   });
