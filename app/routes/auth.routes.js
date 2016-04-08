3438ac030537a5794f6f742eecf797dc58a85b1a
Samuele Zanella <samuele@moku.io>
2016-04-08 11:18:45 +0200

    Fix mistake

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -14,26 +14,25 @@ module.exports = function(apiRoutes) {
         res.json({ success: false, message: 'Authentication failed. User not found.' });
       } else if (member) {
 
-        // check if password matches
-
-      pwdService(req.body.password).verifyAgainst(member.passwordHash, function(error, verified) {
-        if(error)
+        pwdService(req.body.password).verifyAgainst(member.passwordHash, function(error, verified) {
+          if(error)
           res.json({ success: false, message: 'Something went wrong' });
-        else if(!verified) {
-          res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-        } else {
-          var token = jwt.sign(user, app.get('secret'), {
-            expiresInMinutes: 1440 // expires in 24 hours
-          });
+          else if(!verified) {
+            res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+          } else {
+            var token = jwt.sign(user, app.get('secret'), {
+              expiresInMinutes: 1440 // expires in 24 hours
+            });
 
-          // return the information including token as JSON
-          res.json({
-            success: true,
-            message: 'Enjoy your token!',
-            token: token
-          });
-        }
-      });
+            // return the information including token as JSON
+            res.json({
+              success: true,
+              message: 'Enjoy your token!',
+              token: token
+            });
+          }
+        });
+      }
     });
   });
 };
